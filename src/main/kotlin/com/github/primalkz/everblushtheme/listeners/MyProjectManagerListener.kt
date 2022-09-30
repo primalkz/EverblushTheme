d2332e2f61ae7c6d6818ba6c5db0f20630bc7154
GitHub Action <action@github.com>
2022-09-30 10:45:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/primalkz/everblushtheme/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/primalkz/everblushtheme/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.primalkz.everblushtheme.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.primalkz.everblushtheme.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
